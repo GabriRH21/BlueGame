2ab43abfec2b2d0bbbbdf7f8785a3da0ef3bbf34
GabriLaptop <gabriel.rguez.hdez21@gmail.com>
2026-01-31 10:37:27 +0100

    Changing slots builders

--- Assets/Game/Scenes/BoardScene/Scripts/BuilderSlotController.cs
+++ Assets/Game/Scenes/BoardScene/Scripts/BuilderSlotController.cs
@@ -7,13 +7,17 @@ public class BuilderSlotController : MonoBehaviour
 {
     private PieceController _piece;
     [SerializeField] private Button _button;
-    [SerializeField] private BuilderSlotController[] _brothers;
+    [SerializeField] private BuilderSlotController[] _slots;
 
     private void Awake() {
         _button.onClick.AddListener(SlotSelected);
     }
 
     private void SlotSelected() {
+        if (Globals.PlayerStats.PiecesInHand.Count == 0) {
+            return;
+        }
+
         
     }
 }
